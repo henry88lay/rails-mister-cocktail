b1ea2ed13c02c262433b8440925591904e49013f
henry lay <henry88lay@gmail.com>
2020-05-03 13:49:45 +1000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails  do
+  resources :cocktails do
     resources :doses, only: [:create, :new]
   end
 
   resources :doses, only: [:destroy]
-
 end
